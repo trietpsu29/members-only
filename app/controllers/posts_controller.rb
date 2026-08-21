caa7c6727024368c07f8d4609584f644e8c055f8
Triet Psu <112594221+trietpsu29@users.noreply.github.com>
2026-08-21 20:26:04 +0700

    Add posts_controller.rb

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [ :new, :create ]
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+    def post_params
+      params.expect(post: [ :title, :body ])
+    end
+end
