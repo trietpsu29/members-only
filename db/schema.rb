08b0432e36d976db422764375b32aef0671b4302
Triet Psu <112594221+trietpsu29@users.noreply.github.com>
2026-08-21 20:10:54 +0700

    Add User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_21_122825) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_21_125908) do
   create_table "posts", force: :cascade do |t|
     t.text "body", null: false
     t.datetime "created_at", null: false
@@ -18,6 +18,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_21_122825) do
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "posts", "users"
